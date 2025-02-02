8235618a8ff0afbca4903a60fc87a742ea577e62
JakobWiegert <49238974+JakobWiegert@users.noreply.github.com>
2025-02-02 16:11:45 +0100

    added unifi wlc init

---
+++ Docker/config/docker-unifi/init-mongo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+"mongosh" <<EOF
+use ${MONGO_AUTHSOURCE}
+db.auth("${MONGO_INITDB_ROOT_USERNAME}", "${MONGO_INITDB_ROOT_PASSWORD}")
+db.createUser({
+  user: "${MONGO_USER}",
+  pwd: "${MONGO_PASS}",
+  roles: [
+    { db: "${MONGO_DBNAME}", role: "dbOwner" },
+    { db: "${MONGO_DBNAME}_stat", role: "dbOwner" }
+  ]
+})
+EOF
